fa9444ec4f33370fcc5928a28b0f7d2523098b6b
Ian B. S <ianbarbosasantos@gmail.com>
2019-04-14 00:49:33 -0300

    Update conectaBanco.php

--- conectaBanco.php
+++ conectaBanco.php
@@ -1,7 +1,7 @@
 <?php
-$serverName = "localhost";
-$dbname = "bancoLoja";
-$usuario = "root";
-$password = "root";
+$serverName = "lojafaculdade.000webhostapp.com";
+$dbname = "id9290516_lojafaculdade";
+$usuario = "id9290516_lojafaculdadeadmin";
+$password = "lojafacul";
 
 $conn = mysqli_connect($serverName, $usuario, $password, $dbname);
