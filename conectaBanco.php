51b24eacfa5520f86a3d0ad42742f24587301d26
Ian B. S <ianbarbosasantos@gmail.com>
2019-04-14 01:06:55 -0300

    Update conectaBanco.php

--- conectaBanco.php
+++ conectaBanco.php
@@ -1,7 +1,7 @@
 <?php
 $serverName = "remotemysql.com";
 $dbname = "OAqg4ORZFy";
-$usuario = "rEKdBD7Xjy";
-$password = "OAqg4ORZFy";
+$usuario = "OAqg4ORZFy";
+$password = "rEKdBD7Xjy";
 
 $conn = mysqli_connect($serverName, $usuario, $password, $dbname);
